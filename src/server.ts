a4b53c0205736fcf05d48c49363e1c8353da26d3
shoushou <shoushou1106@icloud.com>
2024-12-18 13:16:10 -0800

    feat: Reply to PING and APPLICATION_COMMAND

--- src/server.ts
+++ src/server.ts
@@ -31,13 +31,49 @@ router.get('/', () => {
   return new Response(`Hello World`);
 });
 
-router.post('/', async (request: Request, env) => {
+router.post('/', async (request, env) => {
   const { isValid, interaction } = await server.verifyDiscordRequest(
     request,
-    env
+    env,
   );
   if (!isValid || !interaction) {
     return new Response('Bad request signature.', { status: 401 });
+  }
+
+  if (interaction.type === InteractionType.PING) {
+    // The `PING` message is used during the initial webhook handshake, and is
+    // required to configure the webhook in the developer portal.
+    return new JsonResponse({
+      type: InteractionResponseType.PONG,
+    });
+  }
+
+  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+    // Most user commands will come as `APPLICATION_COMMAND`.
+    switch (interaction.data.name.toLowerCase()) {
+      // case AWW_COMMAND.name.toLowerCase(): {
+      //   const cuteUrl = await getCuteUrl();
+      //   return new JsonResponse({
+      //     type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+      //     data: {
+      //       content: cuteUrl,
+      //     },
+      //   });
+      // }
+      // case INVITE_COMMAND.name.toLowerCase(): {
+      //   const applicationId = env.DISCORD_APPLICATION_ID;
+      //   const INVITE_URL = `https://discord.com/oauth2/authorize?client_id=${applicationId}&scope=applications.commands`;
+      //   return new JsonResponse({
+      //     type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+      //     data: {
+      //       content: INVITE_URL,
+      //       flags: InteractionResponseFlags.EPHEMERAL,
+      //     },
+      //   });
+      // }
+      default:
+        return new JsonResponse({ error: 'Unknown Type' }, { status: 400 });
+    }
   }
 
   console.error('Unknown Type');

2ddec28fd5f9fa9e26d4071cce242c271422673f
shoushou <shoushou1106@icloud.com>
2024-12-17 16:04:48 -0800

    BREAKING CHANGE: apply Discord template

---
+++ src/server.ts
@@ -0,0 +1,68 @@
+/**
+ * The core server that runs on a Cloudflare worker.
+ */
+
+import { AutoRouter } from 'itty-router';
+import {
+  InteractionResponseType,
+  InteractionType,
+  verifyKey,
+} from 'discord-interactions';
+import { InteractionResponseFlags } from 'discord-interactions';
+
+class JsonResponse extends Response {
+	constructor(body: unknown, init?: ResponseInit) {
+	  const jsonBody = JSON.stringify(body);
+	  init = init || {
+		headers: {
+		  'content-type': 'application/json;charset=UTF-8',
+		},
+	  };
+	  super(jsonBody, init);
+	}
+}
+
+const router = AutoRouter();
+
+/**
+ * A simple :wave: hello page to verify the worker is working.
+ */
+router.get('/', (request: Request, env: { DISCORD_APPLICATION_ID: string }) => {
+  return new Response(`👋 ${env.DISCORD_APPLICATION_ID}`);
+});
+
+router.post('/', async (request: Request, env) => {
+  const { isValid, interaction } = await server.verifyDiscordRequest(
+    request,
+    env,
+  );
+  if (!isValid || !interaction) {
+    return new Response('Bad request signature.', { status: 401 });
+  }
+
+  console.error('Unknown Type');
+  return new JsonResponse({ error: 'Unknown Type' }, { status: 400 });
+});
+router.all('*', () => new Response('Not Found.', { status: 404 }));
+
+async function verifyDiscordRequest(request: Request, env: { DISCORD_PUBLIC_KEY: string }) {
+  const signature = request.headers.get('x-signature-ed25519');
+  const timestamp = request.headers.get('x-signature-timestamp');
+  const body = await request.text();
+  const isValidRequest =
+    signature &&
+    timestamp &&
+    (await verifyKey(body, signature, timestamp, env.DISCORD_PUBLIC_KEY));
+  if (!isValidRequest) {
+    return { isValid: false };
+  }
+
+  return { interaction: JSON.parse(body), isValid: true };
+}
+
+const server = {
+  verifyDiscordRequest,
+  fetch: router.fetch,
+};
+
+export default server;
